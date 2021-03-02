9da6b72f1bc1a06d572b799a8432b3c31ba39a6f
jkotima <jkotima@gmail.com>
2021-03-02 13:59:22 +0200

    viikko 3 tehtava 2

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -5,8 +5,8 @@ def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
 
     players = []
 
@@ -15,14 +15,22 @@ def main():
             player_dict['name'],
             player_dict['team'],
             player_dict['goals'],
-            player_dict['assists']
+            player_dict['assists'],
+            player_dict['nationality']
         )
-        if (player_dict['nationality'] == "FIN"):
-            players.append(player)
+        players.append(player)
 
-    print("Oliot:")
+    print("Players:")
 
-    for player in players:
+    finnish_players = list(filter(lambda p: p.nationality == "FIN", players))
+    finnish_players_by_points = sorted(
+        finnish_players,
+        key = lambda p: p.goals + p.assists,
+        reverse=True
+    )
+    
+
+    for player in finnish_players_by_points:
         print(player)
 
 if __name__ == "__main__":
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,11 +1,13 @@
 class Player:
-    def __init__(self, name, team, goals, assists):
+    def __init__(self, name, team, goals, assists, nationality):
         self.name = name
         self.team = team
         self.goals = goals
         self.assists = assists
+        self.nationality = nationality
     
     def __str__(self):
+        '''
         return (
             self.name 
             + " team " + self.team 
@@ -13,3 +15,5 @@ class Player:
             + " assists " + str(self.assists)
 
         )
+        '''
+        return f"{self.name:20} {self.team} {self.assists} + {self.goals} = {self.assists + self.goals}"
