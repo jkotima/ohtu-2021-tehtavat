98f0568beab4cf4d398c0460b405a51686088c59
jkotima <jkotima@gmail.com>
2021-03-02 13:23:27 +0200

    viikko 3 tehtava 1

---
+++ viikko3/nhl-reader/src/index.py
@@ -0,0 +1,29 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'],
+            player_dict['team'],
+            player_dict['goals'],
+            player_dict['assists']
+        )
+        if (player_dict['nationality'] == "FIN"):
+            players.append(player)
+
+    print("Oliot:")
+
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
